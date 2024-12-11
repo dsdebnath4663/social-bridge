ccca5e59d4d7b81f33dbf9c0f143d21153df0a80
debnath <dsdebnath4663@gmail.com>
2024-12-11 20:52:24 +0530

    bug fix alignment of middle bar

--- src/components/HeaderModule.js
+++ src/components/HeaderModule.js
@@ -9,39 +9,39 @@ function HeaderModule() {
     setIsNavOpen(!isNavOpen);
   };
   return (
-    <header class="navbar-light fixed-top header-static">
-      <nav class="navbar navbar-expand-lg bg-white">
-        <div class="container">
-          <a class="btn btn-primary me-2" href="#" role="button"><i class="bi bi-megaphone-fill"></i></a>
+    <header className="navbar-light fixed-top header-static">
+      <nav className="navbar navbar-expand-lg bg-white">
+        <div className="container">
+          <a className="btn btn-primary me-2" href="#" role="button"><i className="bi bi-megaphone-fill"></i></a>
 
-          <a class="btn bg-light navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded={isNavOpen ? "true" : "false"} aria-label="Toggle navigation" onClick={handleToggle} // Handle toggle click
+          <a className="btn bg-light navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded={isNavOpen ? "true" : "false"} aria-label="Toggle navigation" onClick={handleToggle} // Handle toggle click
           >
-            <span className={` toggle-icon pt-1 ${isNavOpen ? '<i class="bi bi-x-lg fs-4"></i>' : '<i class="bi bi-list "></i>'}`}></span>
+            <span className={` toggle-icon pt-1 ${isNavOpen ? '<i className="bi bi-x-lg fs-4"></i>' : '<i className="bi bi-list "></i>'}`}></span>
 
           </a>
 
           {/* navbar-collapse collapse show px-3 py-1 */}
-          <div class="collapse navbar-collapse " id="navbarSupportedContent">
-            <form class="d-flex  position-relative form-padding" role="search">
-              <input class="form-control ps-5 bg-light  " type="search" placeholder="Search" aria-label="Search" />
-              <button class="btn  position-absolute top-1 " type="submit">
-                <i class="bi bi-search"></i>
+          <div className="collapse navbar-collapse " id="navbarSupportedContent">
+            <form className="d-flex  position-relative form-padding" role="search">
+              <input className="form-control ps-5 bg-light  " type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn  position-absolute top-1 " type="submit">
+                <i className="bi bi-search"></i>
               </button>
             </form>
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
 
-              <li class="nav-item dropdown  border-btm  custom-li">
-                <a class="nav-link dropdown-toggle fw-semibold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <li className="nav-item dropdown  border-btm  custom-li">
+                <a className="nav-link dropdown-toggle fw-semibold" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Dropdown
                 </a>
-                <ul class="dropdown-menu bg-light  ">
-                  <li><a class="dropdown-item" href="#">Home default</a></li>
-                  <li><a class="dropdown-item" href="#">Home classic</a></li>
-                  <li><a class="dropdown-item" href="#">Home post</a></li>
-                  <li><a class="dropdown-item" href="#">Home video</a></li>
-                  <li><a class="dropdown-item" href="#">Home event</a></li>
-                  <li><a class="dropdown-item" href="#">Landing page</a></li>
-                  <li><a class="dropdown-item" href="#">App download</a></li>
+                <ul className="dropdown-menu bg-light  ">
+                  <li><a className="dropdown-item" href="#">Home default</a></li>
+                  <li><a className="dropdown-item" href="#">Home classic</a></li>
+                  <li><a className="dropdown-item" href="#">Home post</a></li>
+                  <li><a className="dropdown-item" href="#">Home video</a></li>
+                  <li><a className="dropdown-item" href="#">Home event</a></li>
+                  <li><a className="dropdown-item" href="#">Landing page</a></li>
+                  <li><a className="dropdown-item" href="#">App download</a></li>
                 </ul>
               </li>
 
@@ -52,25 +52,25 @@ function HeaderModule() {
           </div>
 
           {/* hstack gap-2 gap-xl-3 justify-content-center */}
-          <ul class="navbar-nav ms-auto  mb-lg-0 hstack gap-2 gap-xl-3 justify-content-center">
+          <ul className="navbar-nav ms-auto  mb-lg-0 hstack gap-2 gap-xl-3 justify-content-center">
 
-            <li class="nav-item dropdown ">
-              <a class="btn bg-light " href="#" role="button"><i class="bi bi-chat-left-text-fill"></i></a>
+            <li className="nav-item dropdown ">
+              <a className="btn bg-light " href="#" role="button"><i className="bi bi-chat-left-text-fill"></i></a>
             </li>
-            <li class="nav-item dropdown ">
-              <a class="btn bg-light " href="#" role="button"><i class="bi bi-gear"></i></a>
+            <li className="nav-item dropdown ">
+              <a className="btn bg-light " href="#" role="button"><i className="bi bi-gear"></i></a>
             </li>
 
 
 
-            <li class="nav-item dropdown ">
-              <a class="btn bg-light " href="#" role="button">
-                <i class="bi bi-megaphone-fill "></i>
-                <span class="position-absolute top-0 start-100 translate-middle badge border border-light rounded-circle bg-danger p-2"><span class="visually-hidden">unread messages</span></span>
+            <li className="nav-item dropdown ">
+              <a className="btn bg-light " href="#" role="button">
+                <i className="bi bi-megaphone-fill "></i>
+                <span className="position-absolute top-0 start-100 translate-middle badge border border-light rounded-circle bg-danger p-2"><span className="visually-hidden">unread messages</span></span>
               </a>
             </li>
-            <li class="nav-item dropdown ">
-              <a class="btn bg-light   icon-md p-0 " href="#" role="button">
+            <li className="nav-item dropdown ">
+              <a className="btn bg-light   icon-md p-0 " href="#" role="button">
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjZirTv3YUaHSe-VVIQzwXUHXxb8mnJ-krbg&s" className='rounded-2'>
                 </img>
               </a>
--- src/components/MainModule.js
+++ src/components/MainModule.js
@@ -1,61 +1,61 @@
 function MainModule() {
   return (
     <main>
-      <div class="container">
-        <div class="row g-4">
-          <div class="col-lg-3 left-bar ">
+      <div className="container">
+        <div className="row g-4">
+          <div className="col-lg-3 left-bar ">
             <button
-              class="btn btn-primary  d-lg-none d-xl-block d-xxl-none d-md-none d-lg-block"
+              className="btn btn-primary  d-lg-none d-xl-block d-xxl-none d-md-none d-lg-block"
 
               type="button"
               data-bs-toggle="offcanvas"
               data-bs-target="#offcanvasWithBothOptions"
               aria-controls="offcanvasWithBothOptions"
             >
-              <i class="bi bi-sliders"></i>
+              <i className="bi bi-sliders"></i>
             </button>
-            <span class="fw-bold   d-lg-none d-xl-block d-xxl-none d-md-none d-lg-block">My Profile</span>
+            <span className="fw-bold   d-lg-none d-xl-block d-xxl-none d-md-none d-lg-block">My Profile</span>
 
             <div
-              class="offcanvas offcanvas-start "
+              className="offcanvas offcanvas-start "
               data-bs-scroll="true"
               tabindex="-1"
               id="offcanvasWithBothOptions"
               aria-labelledby="offcanvasWithBothOptionsLabel"
             >
-              <div class="offcanvas-header d-lg-none d-xl-block d-xxl-none d-md-none d-lg-block ">
+              <div className="offcanvas-header d-lg-none d-xl-block d-xxl-none d-md-none d-lg-block ">
                 <h5
-                  class="offcanvas-title"
+                  className="offcanvas-title"
                   id="offcanvasWithBothOptionsLabel"
                 ></h5>
                 <button
                   type="button "
-                  class="btn-close "
+                  className="btn-close "
                   data-bs-dismiss="offcanvas"
                   aria-label="Close"
                 ></button>
               </div>
-              <div class="offcanvas-body">
-                <div class="card overflow-hidden  border-0">
-                  <div class="card-img-top mh-100   " alt="..." />
+              <div className="offcanvas-body">
+                <div className="card overflow-hidden  border-0">
+                  <div className="card-img-top mh-100   " alt="..." />
 
-                  <div class="card-body">
+                  <div className="card-body">
                     <div className="text-center">
                       <a
-                        class="btn bg-light profile-pic p-0 "
+                        className="btn bg-light profile-pic p-0 "
                         href="#"
                         role="button"
                       >
                         <img
                           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjZirTv3YUaHSe-VVIQzwXUHXxb8mnJ-krbg&amp;s"
-                          class="rounded border border-white border-3"
+                          className="rounded border border-white border-3"
                         />
                       </a>
-                      <p class="fw-bolder fs-5  mt-3 mb-0">Sam Lanson</p>
-                      <small class=" opacity-50 font">Web Developer at Webestica</small>
+                      <p className="fw-bolder fs-5  mt-3 mb-0">Sam Lanson</p>
+                      <small className=" opacity-50 font">Web Developer at Webestica</small>
 
 
-                      <p class="opacity-75 mt-3 mb-0">
+                      <p className="opacity-75 mt-3 mb-0">
                         I'd love to change the world, but they won’t give me the source code.
 
 
@@ -65,71 +65,71 @@ function MainModule() {
 
                   <div className="     hstack  gap-2 gap-xl-3 justify-content-center mb-3 text-center  mt-minus-3">
                     <div>
-                      <p class="fw-bolder   mb-0">256</p>
+                      <p className="fw-bolder   mb-0">256</p>
                       <small>Post</small>
                     </div>
-                    <div class="vr"></div>
+                    <div className="vr"></div>
                     <div>
-                      <p class="fw-bolder  mb-0">2.5K                      </p>
+                      <p className="fw-bolder  mb-0">2.5K                      </p>
                       <small>Followers</small>
                     </div>
-                    <div class="vr"></div>
+                    <div className="vr"></div>
                     <div>
-                      <p class="fw-bolder  mb-0">256</p>
+                      <p className="fw-bolder  mb-0">256</p>
                       <small>Following</small>
                     </div>
                   </div>
 
-                  <ul class="list-group list-group-flush border-top">
-                    <li class="list-group-item border-0">
+                  <ul className="list-group list-group-flush border-top">
+                    <li className="list-group-item border-0">
                       <a>
-                        <i class="me-2 bi bi-house-heart"></i>
-                        <span class="fw-bolder  mb-0"> Feed</span>
+                        <i className="me-2 bi bi-house-heart"></i>
+                        <span className="fw-bolder  mb-0"> Feed</span>
                       </a>
                     </li>
-                    <li class="list-group-item border-0">
+                    <li className="list-group-item border-0">
                       <a>
-                        <i class=" me-2  bi bi-person-arms-up"></i>
-                        <span class="fw-bolder  mb-0"> Connections</span>
-                      </a>
-                    </li>
-
-                    <li class="list-group-item border-0">
-                      <a>
-                        <i class="me-2 bi bi-globe-americas"></i>
-                        <span class="fw-bolder  mb-0"> Latest News </span>
+                        <i className=" me-2  bi bi-person-arms-up"></i>
+                        <span className="fw-bolder  mb-0"> Connections</span>
                       </a>
                     </li>
 
-                    <li class="list-group-item border-0">
+                    <li className="list-group-item border-0">
                       <a>
-                        <i class="me-2 bi bi-calendar2-event"></i>
-                        <span class="fw-bolder  mb-0"> Event </span>
+                        <i className="me-2 bi bi-globe-americas"></i>
+                        <span className="fw-bolder  mb-0"> Latest News </span>
                       </a>
                     </li>
 
-                    <li class="list-group-item border-0">
+                    <li className="list-group-item border-0">
                       <a>
-                        <i class="me-2  bi bi-chat-quote"></i>
-                        <span class="fw-bolder  mb-0"> Groups</span>
+                        <i className="me-2 bi bi-calendar2-event"></i>
+                        <span className="fw-bolder  mb-0"> Event </span>
                       </a>
                     </li>
 
-                    <li class="list-group-item border-0">
+                    <li className="list-group-item border-0">
                       <a>
-                        <i class="me-2 bi bi-app-indicator"></i>
-                        <span class="fw-bolder  mb-0"> Notifiction</span>
+                        <i className="me-2  bi bi-chat-quote"></i>
+                        <span className="fw-bolder  mb-0"> Groups</span>
                       </a>
                     </li>
-                    <li class="list-group-item border-0">
+
+                    <li className="list-group-item border-0">
                       <a>
-                        <i class="me-2 bi bi-gear"></i>
-                        <span class="fw-bolder  mb-0">  Settings</span>
+                        <i className="me-2 bi bi-app-indicator"></i>
+                        <span className="fw-bolder  mb-0"> Notifiction</span>
+                      </a>
+                    </li>
+                    <li className="list-group-item border-0">
+                      <a>
+                        <i className="me-2 bi bi-gear"></i>
+                        <span className="fw-bolder  mb-0">  Settings</span>
                       </a>
                     </li>
                   </ul>
-                  <div class="card-body text-center  py-2 border-top">
-                    <a href="#" class="card-link text-decoration-none">
+                  <div className="card-body text-center  py-2 border-top">
+                    <a href="#" className="card-link text-decoration-none">
                       View Profile
                     </a>
                   </div>
@@ -139,59 +139,60 @@ function MainModule() {
               </div>
             </div>
           </div>
-          <div class="col-md-8 col-lg-6">Middle Show
-            <div class="card border-0" >
-              <div class="card-body">
-                <div class="d-flex flex-row mb-3">
-                  <div class="p-2 ">
-                    <a class="  btn bg-light   icon-md p-0 " href="#" role="button">
+          <div className="col-md-8 col-lg-6">Middle Show
+            <div className="card border-0" >
+              <div className="card-body">
+                <div className="d-flex flex-row mb-3">
+                  <div className="p-2 ">
+                    <a className="  btn bg-light   icon-md p-0 " href="#" role="button">
                       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjZirTv3YUaHSe-VVIQzwXUHXxb8mnJ-krbg&s" className='rounded-2'>
                       </img>
                     </a>
                   </div>
-                  <div class="p-2 flex-fill">
+                  <div className="p-2 flex-fill">
 
-                    <div class="form-floating ">
-                      <textarea class="form-control border border-0" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+                    <div className="form-floating ">
+                      <textarea className="form-control border border-0" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
                       <label for="floatingTextarea ">Share your thoughts...</label>
                     </div>
                   </div>
 
                 </div>
 
-                <ul class="navbar-nav ms-auto  mb-lg-0 hstack gap-2 gap-xl-3 justify-content-start">
-
-                  <li class="nav-item dropdown ">
-                    <a class="nav-link bg-light d-flex flex-row  text-decoration-none py-1 px-2 rounded" href="#" role="button">
-                      <img src="   https://cdn-icons-png.flaticon.com/512/1375/1375106.png " width="25" height="25" alt="" title="" class="img-small"></img>
+                {/* <ul className="navbar-nav ms-auto  mb-lg-0 hstack gap-2 gap-xl-3 justify-content-start"> */}
+                <ul className="d-flex flex-wrap ps-0">
+                  <li className="nav-item dropdown     list-style-none p-2">
+                    <a className="nav-link bg-light  text-decoration-none py-1 px-2 rounded" href="#" role="button">
+                      <img src="   https://cdn-icons-png.flaticon.com/512/1375/1375106.png " width="25" height="25" alt="" title="" className="img-small"></img>
                       <span className="ms-2">Photo</span>
                     </a>
                   </li>
 
-                  <li class="nav-item dropdown ">
-                    <a class="nav-link bg-light d-flex flex-row  text-decoration-none py-1 px-2 rounded" href="#" role="button">
-                      <img src="    https://cdn-icons-png.flaticon.com/512/2699/2699194.png  " width="25" height="25" alt="" title="" class="img-small"></img>
+                  <li className="nav-item dropdown     list-style-none p-2">
+                    <a className="nav-link bg-light  text-decoration-none py-1 px-2 rounded" href="#" role="button">
+                      <img src="    https://cdn-icons-png.flaticon.com/512/2699/2699194.png  " width="25" height="25" alt="" title="" className="img-small"></img>
                       <span className="ms-2">Video</span>
                     </a>
 
                   </li>
-                  <li class="nav-item dropdown ">
-                    <a class="nav-link bg-light d-flex flex-row  text-decoration-none py-1 px-2 rounded" href="#" role="button">
-                      <img src="   https://cdn-icons-png.flaticon.com/512/1968/1968779.png " width="25" height="25" alt="" title="" class="img-small"></img>
+                  <li className="nav-item dropdown     list-style-none p-2 ">
+                    <a className="nav-link bg-light  text-decoration-none py-1 px-2 rounded" href="#" role="button">
+                      <img src="   https://cdn-icons-png.flaticon.com/512/1968/1968779.png " width="25" height="25" alt="" title="" className="img-small"></img>
                       <span className="ms-2">Event</span>
                     </a>
 
                   </li>
-                  <li class="nav-item dropdown ">
-                    <a class="nav-link bg-light d-flex flex-row  text-decoration-none py-1 px-2 rounded" href="#" role="button">
-                      <img src="   https://cdn-icons-png.flaticon.com/512/1375/1375106.png " width="25" height="25" alt="" title="" class="img-small"></img>
+                  <li className="nav-item dropdown   list-style-none p-2 ">
+                    <a className="nav-link bg-light  text-decoration-none py-1 px-2 rounded" href="#" role="button">
+                      <img src="   https://cdn-icons-png.flaticon.com/512/1375/1375106.png " width="25" height="25" alt="" title="" className="img-small"></img>
                       <span className="ms-2">Feeling /Activity</span>
                     </a>
                   </li>
 
-                  <li class="nav-item dropdown ms-lg-auto">
-                    <a class="nav-link bg-light d-flex flex-row  text-decoration-none py-1 px-2 rounded" href="#" role="button">
-                      <img src="   https://cdn-icons-png.flaticon.com/512/1375/1375106.png " width="25" height="25" alt="" title="" class="img-small"></img>
+                  <li className="nav-item dropdown ms-lg-auto  list-style-none p-2">
+                    <a className="nav-link bg-light  text-decoration-none py-1 px-2 rounded" href="#" role="button">
+                      <i className="bi bi-three-dots"></i>
+                      {/* <img src="   https://cdn-icons-png.flaticon.com/512/1375/1375106.png " width="25" height="25" alt="" title="" className="img-small"></img> */}
                     </a>
                   </li>
 
@@ -200,7 +201,7 @@ function MainModule() {
               </div>
             </div>
           </div>
-          <div class="col-lg-3 d-none d-lg-block">Column</div>{" "}
+          <div className="col-lg-3 d-none d-lg-block">Column</div>{" "}
           {/* Hidden on mobile */}
         </div>
       </div>
