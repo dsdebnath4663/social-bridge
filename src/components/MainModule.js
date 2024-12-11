8b6b501884b3c2e434652bb58a81804fe4e0b6b6
debnath <dsdebnath4663@gmail.com>
2024-12-11 20:25:06 +0530

    Middle bar alignment changes

--- src/components/MainModule.js
+++ src/components/MainModule.js
@@ -36,9 +36,8 @@ function MainModule() {
                 ></button>
               </div>
               <div class="offcanvas-body">
-                <div class="card overflow-hidden">
-                  {/* <div class="mh-100" style="width: 100px; height: 200px;">Max-height 100%</div> */}
-                  <div class="card-img-top mh-100" alt="..." />
+                <div class="card overflow-hidden  border-0">
+                  <div class="card-img-top mh-100   " alt="..." />
 
                   <div class="card-body">
                     <div className="text-center">
@@ -49,7 +48,7 @@ function MainModule() {
                       >
                         <img
                           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjZirTv3YUaHSe-VVIQzwXUHXxb8mnJ-krbg&amp;s"
-                          class="rounded-2"
+                          class="rounded border border-white border-3"
                         />
                       </a>
                       <p class="fw-bolder fs-5  mt-3 mb-0">Sam Lanson</p>
@@ -81,70 +80,131 @@ function MainModule() {
                     </div>
                   </div>
 
-                  <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
+                  <ul class="list-group list-group-flush border-top">
+                    <li class="list-group-item border-0">
                       <a>
                         <i class="me-2 bi bi-house-heart"></i>
                         <span class="fw-bolder  mb-0"> Feed</span>
                       </a>
                     </li>
-                    <li class="list-group-item">
+                    <li class="list-group-item border-0">
                       <a>
                         <i class=" me-2  bi bi-person-arms-up"></i>
                         <span class="fw-bolder  mb-0"> Connections</span>
                       </a>
                     </li>
 
-                    <li class="list-group-item">
+                    <li class="list-group-item border-0">
                       <a>
                         <i class="me-2 bi bi-globe-americas"></i>
                         <span class="fw-bolder  mb-0"> Latest News </span>
                       </a>
                     </li>
 
-                    <li class="list-group-item">
+                    <li class="list-group-item border-0">
                       <a>
                         <i class="me-2 bi bi-calendar2-event"></i>
                         <span class="fw-bolder  mb-0"> Event </span>
                       </a>
                     </li>
 
-                    <li class="list-group-item">
+                    <li class="list-group-item border-0">
                       <a>
                         <i class="me-2  bi bi-chat-quote"></i>
                         <span class="fw-bolder  mb-0"> Groups</span>
                       </a>
                     </li>
 
-                    <li class="list-group-item">
+                    <li class="list-group-item border-0">
                       <a>
                         <i class="me-2 bi bi-app-indicator"></i>
                         <span class="fw-bolder  mb-0"> Notifiction</span>
                       </a>
                     </li>
-                    <li class="list-group-item">
+                    <li class="list-group-item border-0">
                       <a>
                         <i class="me-2 bi bi-gear"></i>
                         <span class="fw-bolder  mb-0">  Settings</span>
                       </a>
                     </li>
                   </ul>
-                  <div class="card-body text-center  py-2">
+                  <div class="card-body text-center  py-2 border-top">
                     <a href="#" class="card-link text-decoration-none">
                       View Profile
                     </a>
                   </div>
+
                 </div>
 
               </div>
             </div>
           </div>
-          <div class="col-md-8 col-lg-6">Middle Show</div>
+          <div class="col-md-8 col-lg-6">Middle Show
+            <div class="card border-0" >
+              <div class="card-body">
+                <div class="d-flex flex-row mb-3">
+                  <div class="p-2 ">
+                    <a class="  btn bg-light   icon-md p-0 " href="#" role="button">
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjZirTv3YUaHSe-VVIQzwXUHXxb8mnJ-krbg&s" className='rounded-2'>
+                      </img>
+                    </a>
+                  </div>
+                  <div class="p-2 flex-fill">
+
+                    <div class="form-floating ">
+                      <textarea class="form-control border border-0" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+                      <label for="floatingTextarea ">Share your thoughts...</label>
+                    </div>
+                  </div>
+
+                </div>
+
+                <ul class="navbar-nav ms-auto  mb-lg-0 hstack gap-2 gap-xl-3 justify-content-start">
+
+                  <li class="nav-item dropdown ">
+                    <a class="nav-link bg-light d-flex flex-row  text-decoration-none py-1 px-2 rounded" href="#" role="button">
+                      <img src="   https://cdn-icons-png.flaticon.com/512/1375/1375106.png " width="25" height="25" alt="" title="" class="img-small"></img>
+                      <span className="ms-2">Photo</span>
+                    </a>
+                  </li>
+
+                  <li class="nav-item dropdown ">
+                    <a class="nav-link bg-light d-flex flex-row  text-decoration-none py-1 px-2 rounded" href="#" role="button">
+                      <img src="    https://cdn-icons-png.flaticon.com/512/2699/2699194.png  " width="25" height="25" alt="" title="" class="img-small"></img>
+                      <span className="ms-2">Video</span>
+                    </a>
+
+                  </li>
+                  <li class="nav-item dropdown ">
+                    <a class="nav-link bg-light d-flex flex-row  text-decoration-none py-1 px-2 rounded" href="#" role="button">
+                      <img src="   https://cdn-icons-png.flaticon.com/512/1968/1968779.png " width="25" height="25" alt="" title="" class="img-small"></img>
+                      <span className="ms-2">Event</span>
+                    </a>
+
+                  </li>
+                  <li class="nav-item dropdown ">
+                    <a class="nav-link bg-light d-flex flex-row  text-decoration-none py-1 px-2 rounded" href="#" role="button">
+                      <img src="   https://cdn-icons-png.flaticon.com/512/1375/1375106.png " width="25" height="25" alt="" title="" class="img-small"></img>
+                      <span className="ms-2">Feeling /Activity</span>
+                    </a>
+                  </li>
+
+                  <li class="nav-item dropdown ms-lg-auto">
+                    <a class="nav-link bg-light d-flex flex-row  text-decoration-none py-1 px-2 rounded" href="#" role="button">
+                      <img src="   https://cdn-icons-png.flaticon.com/512/1375/1375106.png " width="25" height="25" alt="" title="" class="img-small"></img>
+                    </a>
+                  </li>
+
+
+                </ul>
+              </div>
+            </div>
+          </div>
           <div class="col-lg-3 d-none d-lg-block">Column</div>{" "}
           {/* Hidden on mobile */}
         </div>
       </div>
-    </main>
+    </main >
   );
 }
 
